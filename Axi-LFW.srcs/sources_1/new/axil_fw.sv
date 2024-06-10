eb9f96d355af10f184ceb84f3b93438d3116a1f3
grigory <grish.kov@mail.ru>
2024-06-10 15:30:28 +0700

    Update tb_axil_fw_top.sv
    Добавлено: сохранение адреса ошибки в карту регистров.
    Исправлено: завершение транзакции (раньше - через установку valid мастера, что неправильно, сейчас установка ready  слейва)

--- Axi-LFW.srcs/sources_1/new/axil_fw.sv
+++ Axi-LFW.srcs/sources_1/new/axil_fw.sv
@@ -4,8 +4,8 @@ module axil_fw #(
 
     parameter   G_CLK           = 1.0,          //  clock
                 G_WD_TIM        = 50,           //  watchDog timer
-                G_CNT_WDT       = 4             //  counter width
-
+                G_CNT_WDT       = 4,            //  counter width
+                G_ADDR_W        = 12            //  AXIL xADDR bit width
 
 ) (
 
@@ -36,39 +36,46 @@ module axil_fw #(
     assign m_axil.rvalid    = s_axil.rvalid;
     assign m_axil.rdata     = s_axil.rdata;
 
-    assign m_axil.bresp     = 2'b00;            //  set bresp always OK
-    assign m_axil.rresp     = 2'b00;            //  set rresp always OK
+    assign m_axil.bresp     = 2'b00;                    //  set bresp always OK
+    assign m_axil.rresp     = 2'b00;                    //  set rresp always OK
 
-    reg [G_CNT_WDT : 0] q_w_err_cnt     = 0,    //  write error counter
-                        q_r_err_cnt     = 0,    //  read error counter
-                        q_wd_err_cnt    = 0;    //  watchDog error counter
+    reg [G_ADDR_W - 1 : 0]  xADDR = '1;
 
-    reg [10 : 0] q_wd_cnt = 0;                  //  watchDog counter
+    reg [G_CNT_WDT : 0]     q_w_err_cnt     = 0,        //  write error counter
+                            q_r_err_cnt     = 0,        //  read error counter
+                            q_wd_err_cnt    = 0;        //  watchdog error counter
 
-    reg [1 : 0] q_bresp = '0,                   //  temp. bresp register
-                q_rresp = '0;                   //  temp. rresp register
+    reg [10 : 0] q_wd_cnt = 0;                          //  watchdog counter
 
-    logic q_wd_ena = 0;                         //  enable WatchDog, active - high
+    reg [1 : 0] q_bresp = '0,                           //  temp. bresp register
+                q_rresp = '0;                           //  temp. rresp register
 
-    reg [(G_CNT_WDT * 3) + 2 : 0] RG_STAT;      //  reg. map
+    logic   q_wd_ena = 0;                               //  enable WatchDog, active - high
 
-    assign RG_STAT [G_CNT_WDT : 0]
-                    = q_w_err_cnt;              //  write error
+    logic   q_w_ena = 0,                                //  ???
+            q_r_ena = 0;                                //  ???
 
-    assign RG_STAT [((G_CNT_WDT * 2) + 1) : (G_CNT_WDT + 1)] 
-                    = q_r_err_cnt;              //  read error
+    reg [G_ADDR_W + 2 + G_CNT_WDT * 3 : 0] RG_STAT;     //  reg. map of errors
 
-    assign RG_STAT [((G_CNT_WDT * 3) + 2) : ((G_CNT_WDT * 2) + 2)] 
-                    = q_wd_err_cnt;             //  watchDog error
+    assign RG_STAT [G_ADDR_W + 2 + G_CNT_WDT * 3 : G_CNT_WDT * 3 + 3] = 
+            (q_bresp != 2'b00 | q_rresp != 2'b00 | q_wd_err_cnt != 0) ? xADDR : '0;     //  error address
 
-    always_ff @(posedge i_clk) begin
+    assign RG_STAT [G_CNT_WDT * 3 + 2 : 
+                    G_CNT_WDT * 2 + 2]  = q_w_err_cnt;              //  write error
+
+    assign RG_STAT [G_CNT_WDT * 2 + 1 : 
+                    G_CNT_WDT + 1]      = q_r_err_cnt;              //  read error
+
+    assign RG_STAT [G_CNT_WDT : 0]      = q_wd_err_cnt;             //  watchdog error 
+
+    always_ff @(posedge i_clk) begin 
 
         // counting write errors
 
         if (q_bresp != 2'b00) begin
 
             q_w_err_cnt <= q_w_err_cnt + 1; 
-            q_bresp <= 2'b00;                   
+            q_bresp     <= 2'b00;                   
 
         end
 
@@ -77,14 +84,20 @@ module axil_fw #(
          if (q_rresp != 2'b00) begin
 
             q_r_err_cnt <= q_r_err_cnt + 1;
-            q_rresp <= 2'b00;
+            q_rresp     <= 2'b00;
 
         end
-            
-        // enable watchdog and start counting
+
+        if (!q_w_ena & !s_axil.bready)
+            s_axil.bvalid   <= 0;
+
+        if (!q_r_ena & !s_axil.rready)
+            s_axil.rvalid   <= 0;
+
+        // if watchdog is enable, start counting
 
         if (q_wd_ena)
-            q_wd_cnt <= q_wd_cnt + 1;
+            q_wd_cnt    <= q_wd_cnt + 1;
 
         // counting until max timer value
 
@@ -94,38 +107,63 @@ module axil_fw #(
             q_wd_ena        <= 0;        
             q_wd_cnt        <= 0;
 
-            //  if write error, closing transaction by setting awready to 1 and bresp to slave error
+            //  if write error, close transaction with slave error
 
-            if (s_axil.awvalid) begin
+            if (q_w_ena) begin
                 
-                s_axil.awready  <= 1;
-                s_axil.bresp    <= 2'b10;
+                q_bresp         <= 2'b10; 
+
+                if (s_axil.wvalid) 
+                    s_axil.wready   <= 1;
+
+                s_axil.bvalid   <= 1;
 
             end
 
-            //  if read error, closing transaction by setting arready to 1 and rresp to slave error
+            //  if read error, close transaction with slave error
 
-            if (s_axil.arvalid) begin
+            if (q_r_ena) begin
 
-                s_axil.arready  <= 1;
-                s_axil.rresp    <= 2'b10;
+                q_rresp         <= 2'b10; 
+                
+                if (s_axil.rready) 
+                    s_axil.rvalid   <= 1;
+                
+                q_r_ena         <= 0;
 
             end
 
         end
 
-        //  if start of transaction, enable watchdog  
+        //  if start of write transaction, enable watchdog
 
-        if ((s_axil.awvalid & s_axil.awready) | (s_axil.arvalid & s_axil.arready))
-            q_wd_ena <= 1;
+        if (s_axil.awvalid & s_axil.awready) begin
+
+            q_wd_ena    <= 1;
+            q_w_ena     <= 1;
+            xADDR       <= s_axil.awaddr;
+
+        end
+
+        //  if start of read transaction, enable watchdog
+
+        if (s_axil.arvalid & s_axil.arready) begin
+
+            q_wd_ena    <= 1;
+            q_r_ena     <= 1;
+            xADDR       <= s_axil.araddr;
+
+        end
 
         //  if end of write transaction, reset watchdog
 
         if (s_axil.bvalid & s_axil.bready) begin
 
-            q_bresp     <= s_axil.bresp;
-            q_wd_cnt    <= 0;
-            q_wd_ena    <= 0;
+            q_bresp         <= s_axil.bresp;
+            q_wd_cnt        <= 0;
+            q_wd_ena        <= 0;
+            q_w_ena         <= 0;
+            s_axil.wready   <= 0;
 
         end
         
@@ -136,15 +174,18 @@ module axil_fw #(
             q_rresp     <= s_axil.rresp;
             q_wd_cnt    <= 0;
             q_wd_ena    <= 0;
+            q_r_ena     <= 0;
 
         end
 
+        //  reset, active - high
+
         if (i_rst) begin
 
-            q_wd_cnt    <= 0;
-            q_wd_ena    <= 0;
-            q_rresp     <= 2'b00;
-            q_bresp     <= 2'b00;
+            q_wd_cnt            <= 0;
+            q_wd_ena            <= 0;
+            q_rresp             <= 2'b00;
+            q_bresp             <= 2'b00;
 
         end
 
