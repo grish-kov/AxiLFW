f6ac7b6432be84dbcd8cc27c64f6b5c1baaeba38
grigory <grish.kov@mail.ru>
2024-06-19 10:10:22 +0700

    Refactoring for use with IPs

--- Axi-LFW.srcs/sim_1/new/tb_axil_fw_top.sv
+++ Axi-LFW.srcs/sim_1/new/tb_axil_fw_top.sv
@@ -2,17 +2,19 @@
 
 module tb_axil_fw_top #(
 
-	int G_ADDR_W = 4, 					// AXIL xADDR bit width
-	int G_DATA_B = 8, 					// AXIL xDATA number of bytes (B)
-	
+	int G_ADDR_W	= 8, 					// AXIL xADDR bit width
+		G_DATA_B 	= 8, 					// AXIL xDATA number of bytes (B)
+		G_WD_WDT 	= 8, 
 	real dt = 1.0 						// clock period ns
 	);
 
-	localparam C_DATA_W = 8 * G_DATA_B; // AXIL xDATA bit width
+	localparam C_DATA_W = G_DATA_B << 3; // AXIL xDATA bit width
 
-	logic i_clk   	= 1;
-	logic i_rst_n 	= 0;
-	logic i_err 	= 0;
+	logic 	i_clk   = 1,
+			i_rst	= 0,
+			i_err 	= 0;
+
+	reg [G_WD_WDT - 1 : 0] i_len = 8;
 
 	reg	[4 : 0] i_hsk_ena 	= '1; 
 
@@ -20,8 +22,8 @@ module tb_axil_fw_top #(
 
 	initial begin
 
-		i_rst_n = 1; #3;
-		i_rst_n = 0;
+		i_rst = 1; #3;
+		i_rst = 0;
 
 	end
 
@@ -30,8 +32,13 @@ module tb_axil_fw_top #(
 		.A(G_ADDR_W), 
 		.PAYMASK(5'b01101)) m_axil ();
 
-	typedef logic [G_ADDR_W-1:0] t_xaddr;
-	typedef logic [C_DATA_W-1:0] t_xdata;
+	if_axil #(
+		.N(G_DATA_B), 
+		.A(G_ADDR_W), 
+		.PAYMASK(5'b01101)) ctrl_axil ();
+
+	typedef logic [G_ADDR_W - 1 : 0] t_xaddr;
+	typedef logic [C_DATA_W - 1 : 0] t_xdata;
 
 	task t_axil_m_init;
 		begin
@@ -46,6 +53,20 @@ module tb_axil_fw_top #(
 			m_axil.rready  = '1;
 		end
 	endtask : t_axil_m_init
+
+	task t_axil_ctrl_init;
+		begin
+			ctrl_axil.awvalid = '0;
+			ctrl_axil.awaddr  = '0;
+			ctrl_axil.wvalid  = '0;
+			ctrl_axil.wdata   = '0;
+			ctrl_axil.wstrb   = '0;
+			ctrl_axil.bready  = '1;
+			ctrl_axil.arvalid = '0;
+			ctrl_axil.araddr  = '0;
+			ctrl_axil.rready  = '1;
+		end
+	endtask : t_axil_ctrl_init
 
 	`define MACRO_AXIL_HSK(name, miso, mosi) \
 		``name``.``mosi``= '1; \
@@ -85,6 +106,19 @@ module tb_axil_fw_top #(
 		end
 	endtask : t_axil_m_rd	
 
+	task t_axil_ctrl_rd;
+		input  t_xaddr ADDR;
+		begin
+
+		// read address
+			ctrl_axil.araddr = ADDR;
+			`MACRO_AXIL_HSK(ctrl_axil, arready, arvalid);
+		// read data
+			// `MACRO_AXIL_HSK(m_axil, rvalid, rready);
+		
+		end
+	endtask : t_axil_ctrl_rd	
+
  	localparam t_xaddr	TST_ADDR1   = 'h01; 
     localparam t_xaddr 	TST_ADDR2	= 'h02;
 	localparam t_xaddr  TST_ADDR3   = 'h03;
@@ -96,31 +130,43 @@ module tb_axil_fw_top #(
 
 	initial begin
 
+		t_axil_ctrl_init;
 		t_axil_m_init; #10.9;
 
+		i_hsk_ena[0] = 0;
+		t_axil_m_wr(.ADDR(TST_ADDR1), .DATA(111)); 			// 1
+
+		#60; i_hsk_ena[0] = 1;
+
 		i_err = 1;
-		t_axil_m_wr(.ADDR(TST_ADDR1), .DATA(111)); 	#10;			// 1
-		i_err = 0;				
 		t_axil_m_wr(.ADDR(TST_ADDR2), .DATA(222)); 	#10;			// 2
+		i_err = 0;
 		t_axil_m_wr(.ADDR(WRN_ADDR1), .DATA(333));	#10;			// 5
 		t_axil_m_wr(.ADDR(WRN_ADDR2), .DATA(333)); 	#10;			// 6	
 		t_axil_m_wr(.ADDR(TST_ADDR3), .DATA(333)); 	#10;			// 7
 
-		t_axil_m_rd(.ADDR(TST_ADDR4)); 				#10;			// 8
-		t_axil_m_rd(.ADDR(WRN_ADDR2)); 				#10;			// 9
-		t_axil_m_rd(.ADDR(TST_ADDR1)); 				#10;			// 10
+		t_axil_ctrl_rd(.ADDR('h01)); 				#10;			// 8
+		t_axil_ctrl_rd(.ADDR('h02)); 				#10;			// 9
+		t_axil_ctrl_rd(.ADDR('h03)); 				#10;			// 10
 
 	end
 
-    axil_fw_top u_axil_fw  (
+    axil_fw_top#(
+
+        .G_ADDR_W (G_ADDR_W),
+        .G_DATA_B (G_DATA_B)
+		
+		) u_axil_fw  (
 
         .i_clk      	(i_clk),
-        .i_rst      	(i_rst_n),
+        .i_rst      	(i_rst),
+		.i_len			(i_len),
 
 		.i_err          (i_err),
         .i_hsk_ena      (i_hsk_ena),
 
-        .s_axil     	(m_axil)
+        .s_axil     	(m_axil),
+		.ctrl_axil		(ctrl_axil)
 
 	);
 
--- Axi-LFW.srcs/sources_1/new/axil_fw_top.sv
+++ Axi-LFW.srcs/sources_1/new/axil_fw_top.sv
@@ -1,37 +1,323 @@
 `timescale 1ns / 1ps
 
+package axi;
+
+  // AXI4-Lite assignment macros
+  // Example: `AXIL_ASSIGN(assign, s_axil_, =, s_axil_); or `AXIL_ASSIGN(assign, m_axif_, =, m_axif.);
+  `define AXIL_ASSIGN_WA(statement, lhs, operator, rhs) \
+    statement ``lhs``awvalid operator ``rhs``awvalid; \
+    statement ``rhs``awready operator ``lhs``awready; \
+    statement ``lhs``awaddr operator ``rhs``awaddr; \
+    statement ``lhs``awprot operator ``rhs``awprot;
+
+  `define AXIL_ASSIGN_WD(statement, lhs, operator, rhs) \
+    statement ``lhs``wvalid operator ``rhs``wvalid; \
+    statement ``rhs``wready operator ``lhs``wready; \
+    statement ``lhs``wdata operator ``rhs``wdata; \
+    statement ``lhs``wstrb operator ``rhs``wstrb;
+
+  `define AXIL_ASSIGN_WR(statement, lhs, operator, rhs) \
+    statement ``lhs``bvalid operator ``rhs``bvalid; \
+    statement ``rhs``bready operator ``lhs``bready; \
+    statement ``lhs``bresp operator ``rhs``bresp;
+    
+  `define AXIL_ASSIGN_RA(statement, lhs, operator, rhs) \
+    statement ``lhs``arvalid operator ``rhs``arvalid; \
+    statement ``rhs``arready operator ``lhs``arready; \
+    statement ``lhs``araddr operator ``rhs``araddr; \
+    statement ``lhs``arprot operator ``rhs``arprot;
+
+  `define AXIL_ASSIGN_RD(statement, lhs, operator, rhs) \
+    statement ``lhs``rvalid operator ``rhs``rvalid; \
+    statement ``rhs``rready operator ``lhs``rready; \
+    statement ``lhs``rdata operator ``rhs``rdata; \
+    statement ``lhs``rresp operator ``rhs``rresp;
+
+  `define AXIL_ASSIGN_W(statement, lhs, operator, rhs) \
+    `AXIL_ASSIGN_WA(statement, lhs, operator, rhs); \
+    `AXIL_ASSIGN_WD(statement, lhs, operator, rhs); \
+    `AXIL_ASSIGN_WR(statement, rhs, operator, lhs);
+    
+  `define AXIL_ASSIGN_R(statement, lhs, operator, rhs) \
+    `AXIL_ASSIGN_RA(statement, lhs, operator, rhs); \
+    `AXIL_ASSIGN_RD(statement, rhs, operator, lhs);
+
+  `define AXIL_ASSIGN(statement, lhs, operator, rhs) \
+    `AXIL_ASSIGN_W(statement, lhs, operator, rhs); \
+    `AXIL_ASSIGN_R(statement, lhs, operator, rhs);
+  
+  // Types
+  typedef logic [3:0] t_region;
+  typedef logic [7:0] t_len;
+  typedef logic [2:0] t_size;
+  typedef logic [1:0] t_burst;
+  typedef logic [0:0] t_lock;
+  typedef logic [3:0] t_cache;
+  typedef logic [2:0] t_prot;
+  typedef logic [3:0] t_qos;
+  typedef logic [1:0] t_resp;
+
+  typedef logic [0:0] t_last;
+
+  typedef struct packed { bit AWPROT, WSTRB, BRESP, ARPROT, RRESP; } t_axil_mask;
+
+  // Constants
+  localparam t_burst AXI_BURST_FIXED = 2'b00;
+  localparam t_burst AXI_BURST_INCR = 2'b01;
+  localparam t_burst AXI_BURST_WRAP = 2'b10;
+
+  localparam t_resp AXI_RESP_OKAY = 2'b00;
+  localparam t_resp AXI_RESP_EXOKAY = 2'b01;
+  localparam t_resp AXI_RESP_SLVERR = 2'b10;
+  localparam t_resp AXI_RESP_DECERR = 2'b11;
+  
+endpackage : axi
+
+//  AXI4 Memory Mapped (lite) Parameters to Define the Signal Widths:
+//    N - Data Bus Width in Bytes;
+//    A - Address Width;
+//    PAYMASK - Payload Mask: { awprot, wstrb, bresp, arprot, rresp }
+
+interface if_axil #( parameter int N = 8, A = 16, bit [0:4] PAYMASK = '1 ) ();
+
+  import axi::*;
+  
+  typedef logic [N*8 - 1:0] t_data;
+  typedef logic [N-1:0] t_strb;
+  typedef logic [A-1:0] t_addr;
+
+  // Check Parameters
+  initial begin : check_param
+    if ( !(N inside {4, 8, 16}) )
+      $error("[%s %0d-%0d] Write and read data bus width (%0d) in AXI4-Lite interface must be equal to 4, 8 or 16 bytes. %m", "AXI", 3, 1, N);
+    if ( A < 1 || A > 64 )
+      $error("[%s %0d-%0d] Write and read address bus width (%0d) in AXI4-Lite interface must be greater than or equal to 1 and less than or equal to 64. %m", "AXI", 3, 2, A);
+  end : check_param
+
+  // Write Address Channel Signals
+  logic   awvalid;
+  logic   awready;
+  t_addr  awaddr;
+  t_prot  awprot;
+
+  // Write Data Channel Signals
+  logic   wvalid;
+  logic   wready;
+  t_data  wdata;
+  t_strb  wstrb;
+
+  // Write Response Channel Signals
+  logic   bvalid;
+  logic   bready;
+  t_resp  bresp;
+
+  // Read Address Channel Signals
+  logic   arvalid;
+  logic   arready;
+  t_addr  araddr;
+  t_prot  arprot;
+
+  // Read Data Channel Signals
+  logic   rvalid;
+  logic   rready;
+  t_data  rdata;
+  t_resp  rresp;
+
+  typedef struct packed { bit AWPROT, WSTRB, BRESP, ARPROT, RRESP; } t_msk;
+
+  localparam t_msk MASK = PAYMASK;
+
+  // Write Address Payload
+  localparam int WA_PAYLOAD = A + MASK.AWPROT*3;
+
+  localparam int AWADDR_OFFSET = A;
+
+  function logic [WA_PAYLOAD-1:0] wa_payload();
+    wa_payload = awaddr;
+    if ( MASK.AWPROT ) wa_payload = awprot << AWADDR_OFFSET | wa_payload;
+  endfunction : wa_payload
+
+  function void wa_paymask( input logic [WA_PAYLOAD-1:0] payload );
+    awaddr = payload;
+    awprot = ( MASK.AWPROT ) ? payload >> AWADDR_OFFSET : '0;
+  endfunction : wa_paymask
+
+  // Write Data Payload
+  localparam int WD_PAYLOAD = N*8 + MASK.WSTRB*N;
+
+  localparam int WDATA_OFFSET = N*8;
+
+  function logic [WD_PAYLOAD-1:0] wd_payload();
+    wd_payload = wdata;
+    if ( MASK.WSTRB ) wd_payload = wstrb << WDATA_OFFSET | wd_payload;
+  endfunction : wd_payload
+
+  function void wd_paymask( input logic [WD_PAYLOAD-1:0] payload );
+    wdata = payload;
+    wstrb = ( MASK.WSTRB ) ? payload >> WDATA_OFFSET : '1;
+  endfunction : wd_paymask
+
+  // Write Response Payload
+  localparam int WR_PAYLOAD = MASK.BRESP*2;
+
+  function void wr_paymask( input logic [WR_PAYLOAD-1:0] payload );
+    bresp = ( MASK.BRESP ) ? payload : AXI_RESP_OKAY;
+  endfunction : wr_paymask
+
+  function logic [WR_PAYLOAD-1:0] wr_payload();
+    if ( MASK.BRESP ) wr_payload = bresp;
+  endfunction : wr_payload
+
+  // Read Address Payload
+  localparam int RA_PAYLOAD = A + MASK.ARPROT*3;
+
+  localparam int ARADDR_OFFSET = A;
+
+  function logic [RA_PAYLOAD-1:0] ra_payload();
+    ra_payload = araddr;
+    if ( MASK.ARPROT ) ra_payload = arprot << ARADDR_OFFSET | ra_payload;
+  endfunction : ra_payload
+
+  function void ra_paymask( input logic [RA_PAYLOAD-1:0] payload );
+    araddr = payload;
+    arprot = ( MASK.ARPROT ) ? payload >> ARADDR_OFFSET : '0;
+  endfunction : ra_paymask
+
+  // Read Data Payload
+  localparam int RD_PAYLOAD = N*8 + MASK.RRESP*2;
+
+  localparam int RDATA_OFFSET = N*8;
+
+  function void rd_paymask( input logic [RD_PAYLOAD-1:0] payload );
+    rdata = payload;
+    rresp = ( MASK.RRESP ) ? payload >> RDATA_OFFSET : AXI_RESP_OKAY;
+  endfunction : rd_paymask
+
+  function logic [RD_PAYLOAD-1:0] rd_payload();
+    rd_payload = rdata;
+    if ( MASK.RRESP ) rd_payload = rresp << RDATA_OFFSET | rd_payload;
+  endfunction : rd_payload
+
+  // AXI-Lite Payload
+  localparam int PAYLOAD = WA_PAYLOAD + WD_PAYLOAD + WR_PAYLOAD + RA_PAYLOAD + RD_PAYLOAD;
+
+  // Master Ports
+  modport master (
+    input awready, output awvalid, awaddr, awprot, 
+    input wready, output wvalid, wdata, wstrb, 
+    output bready, input bvalid, bresp, 
+    input arready, output arvalid, araddr, arprot, 
+    output rready, input rvalid, rdata, rresp, 
+    import wa_paymask, import wd_paymask, import wr_payload, import ra_paymask, import rd_payload 
+  );
+
+  // Slave Ports
+  modport slave (
+    output awready, input awvalid, awaddr, awprot, 
+    output wready, input wvalid, wdata, wstrb, 
+    input bready, output bvalid, bresp, 
+    output arready, input arvalid, araddr, arprot, 
+    input rready, output rvalid, rdata, rresp, 
+    import wa_payload, import wd_payload, import wr_paymask, import ra_payload, import rd_paymask
+  );
+
+endinterface : if_axil
+
 module axil_fw_top #(
 
-    int G_ADDR_W = 4, 		        // AXIL xADDR bit width
-	int G_DATA_B = 8 		        // AXIL xDATA number of bytes (B)
+    int G_ADDR_W = 4, 		            // AXIL xADDR bit width
+        G_DATA_B = 8, 		            // AXIL xDATA number of bytes (B)
+        G_WD_WDT = 8 
 
 ) (
 
     input           i_clk,          
                     i_rst,
+        reg [G_WD_WDT - 1 : 0]     i_len,
 
-        reg [1 : 0] i_err,
+        reg         i_err,
             [4 : 0] i_hsk_ena,          
 
-    if_axil.s       s_axil         // Receive from Zynq
+    if_axil.slave       s_axil,         //  receive from Zynq
+    if_axil.slave       ctrl_axil       //  control fw 
 
     );
 
     if_axil #(
-		.N(G_DATA_B), 
-		.A(G_ADDR_W), 
-		.PAYMASK(5'b01101)
-        ) m_axil ();
 
-    axil_fw u_axil_fw(
+      .N(G_DATA_B), 
+      .A(G_ADDR_W), 
+      .PAYMASK(5'b01101)
 
-        .i_clk              (i_clk),
-        .i_rst              (i_rst),
+      ) m_axil ();
 
-        .s_axil             (s_axil),
-        .m_axil             (m_axil)
+    axil_fw u_fw (
 
-    );
+        .i_clk                          (i_clk),                    // input wire i_clk
+        .i_rst                          (i_rst),                    // input wire i_rst
+        .i_len                          (i_len),
+          
+        .m_axil_awready                 (m_axil.awready),  // input wire m_axil_awready
+        .m_axil_awvalid                 (m_axil.awvalid),  // output wire m_axil_awvalid
+        .m_axil_awaddr                  (m_axil.awaddr),    // output wire [7 : 0] m_axil_awaddr
+        .m_axil_awprot                  (m_axil.awprot),    // output wire [2 : 0] m_axil_awprot
+        .m_axil_wready                  (m_axil.wready),    // input wire m_axil_wready
+        .m_axil_wvalid                  (m_axil.wvalid),    // output wire m_axil_wvalid
+        .m_axil_wdata                   (m_axil.wdata),      // output wire [63 : 0] m_axil_wdata
+        .m_axil_wstrb                   (m_axil.wstrb),      // output wire [7 : 0] m_axil_wstrb
+        .m_axil_bready                  (m_axil.bready),    // output wire m_axil_bready
+        .m_axil_bvalid                  (m_axil.bvalid),    // input wire m_axil_bvalid
+        .m_axil_bresp                   (m_axil.bresp),      // input wire [1 : 0] m_axil_bresp
+        .m_axil_arready                 (m_axil.arready),  // input wire m_axil_arready
+        .m_axil_arvalid                 (m_axil.arvalid),  // output wire m_axil_arvalid
+        .m_axil_araddr                  (m_axil.araddr),    // output wire [7 : 0] m_axil_araddr
+        .m_axil_arprot                  (m_axil.arprot),    // output wire [2 : 0] m_axil_arprot
+        .m_axil_rready                  (m_axil.rready),    // output wire m_axil_rready
+        .m_axil_rvalid                  (m_axil.rvalid),    // input wire m_axil_rvalid
+        .m_axil_rdata                   (m_axil.rdata),      // input wire [63 : 0] m_axil_rdata
+        .m_axil_rresp                   (m_axil.rresp),      // input wire [1 : 0] m_axil_rresp
+          
+        .s_axil_awready                 (s_axil.awready),  // output wire s_axil_awready
+        .s_axil_awvalid                 (s_axil.awvalid),  // input wire s_axil_awvalid
+        .s_axil_awaddr                  (s_axil.awaddr),    // input wire [7 : 0] s_axil_awaddr
+        .s_axil_awprot                  (s_axil.awprot),    // input wire [2 : 0] s_axil_awprot
+        .s_axil_wready                  (s_axil.wready),    // output wire s_axil_wready
+        .s_axil_wvalid                  (s_axil.wvalid),    // input wire s_axil_wvalid
+        .s_axil_wdata                   (s_axil.wdata),      // input wire [63 : 0] s_axil_wdata
+        .s_axil_wstrb                   (s_axil.wstrb),      // input wire [7 : 0] s_axil_wstrb
+        .s_axil_bready                  (s_axil.bready),    // input wire s_axil_bready
+        .s_axil_bvalid                  (s_axil.bvalid),    // output wire s_axil_bvalid
+        .s_axil_bresp                   (s_axil.bresp),      // output wire [1 : 0] s_axil_bresp
+        .s_axil_arready                 (s_axil.arready),  // output wire s_axil_arready
+        .s_axil_arvalid                 (s_axil.arvalid),  // input wire s_axil_arvalid
+        .s_axil_araddr                  (s_axil.araddr),    // input wire [7 : 0] s_axil_araddr
+        .s_axil_arprot                  (s_axil.arprot),    // input wire [2 : 0] s_axil_arprot
+        .s_axil_rready                  (s_axil.rready),    // input wire s_axil_rready
+        .s_axil_rvalid                  (s_axil.rvalid),    // output wire s_axil_rvalid
+        .s_axil_rdata                   (s_axil.rdata),      // output wire [63 : 0] s_axil_rdata
+        .s_axil_rresp                   (s_axil.rresp),      // output wire [1 : 0] s_axil_rresp
+
+        .ctrl_axil_awready                 (ctrl_axil.awready),  // output wire s_axil_awready
+        .ctrl_axil_awvalid                 (ctrl_axil.awvalid),  // input wire s_axil_awvalid
+        .ctrl_axil_awaddr                  (ctrl_axil.awaddr),    // input wire [7 : 0] s_axil_awaddr
+        .ctrl_axil_awprot                  (ctrl_axil.awprot),    // input wire [2 : 0] s_axil_awprot
+        .ctrl_axil_wready                  (ctrl_axil.wready),    // output wire s_axil_wready
+        .ctrl_axil_wvalid                  (ctrl_axil.wvalid),    // input wire s_axil_wvalid
+        .ctrl_axil_wdata                   (ctrl_axil.wdata),      // input wire [63 : 0] s_axil_wdata
+        .ctrl_axil_wstrb                   (ctrl_axil.wstrb),      // input wire [7 : 0] s_axil_wstrb
+        .ctrl_axil_bready                  (ctrl_axil.bready),    // input wire s_axil_bready
+        .ctrl_axil_bvalid                  (ctrl_axil.bvalid),    // output wire s_axil_bvalid
+        .ctrl_axil_bresp                   (ctrl_axil.bresp),      // output wire [1 : 0] s_axil_bresp
+        .ctrl_axil_arready                 (ctrl_axil.arready),  // output wire s_axil_arready
+        .ctrl_axil_arvalid                 (ctrl_axil.arvalid),  // input wire s_axil_arvalid
+        .ctrl_axil_araddr                  (ctrl_axil.araddr),    // input wire [7 : 0] s_axil_araddr
+        .ctrl_axil_arprot                  (ctrl_axil.arprot),    // input wire [2 : 0] s_axil_arprot
+        .ctrl_axil_rready                  (ctrl_axil.rready),    // input wire s_axil_rready
+        .ctrl_axil_rvalid                  (ctrl_axil.rvalid),    // output wire s_axil_rvalid
+        .ctrl_axil_rdata                   (ctrl_axil.rdata),      // output wire [63 : 0] s_axil_rdata
+        .ctrl_axil_rresp                   (ctrl_axil.rresp)      // output wire [1 : 0] s_axil_rresp
+        
+);
 
     fw_reg_map u_rg_map(
 
@@ -40,7 +326,25 @@ module axil_fw_top #(
         .i_err              (i_err),
         .i_hsk_ena          (i_hsk_ena),
 
-        .s_axil             (m_axil)
+        .s_axil_awready                 (m_axil.awready),  // output wire s_axil_awready
+        .s_axil_awvalid                 (m_axil.awvalid),  // input wire s_axil_awvalid
+        .s_axil_awaddr                  (m_axil.awaddr),    // input wire [7 : 0] s_axil_awaddr
+        .s_axil_awprot                  (m_axil.awprot),    // input wire [2 : 0] s_axil_awprot
+        .s_axil_wready                  (m_axil.wready),    // output wire s_axil_wready
+        .s_axil_wvalid                  (m_axil.wvalid),    // input wire s_axil_wvalid
+        .s_axil_wdata                   (m_axil.wdata),      // input wire [63 : 0] s_axil_wdata
+        .s_axil_wstrb                   (m_axil.wstrb),      // input wire [7 : 0] s_axil_wstrb
+        .s_axil_bready                  (m_axil.bready),    // input wire s_axil_bready
+        .s_axil_bvalid                  (m_axil.bvalid),    // output wire s_axil_bvalid
+        .s_axil_bresp                   (m_axil.bresp),      // output wire [1 : 0] s_axil_bresp
+        .s_axil_arready                 (m_axil.arready),  // output wire s_axil_arready
+        .s_axil_arvalid                 (m_axil.arvalid),  // input wire s_axil_arvalid
+        .s_axil_araddr                  (m_axil.araddr),    // input wire [7 : 0] s_axil_araddr
+        .s_axil_arprot                  (m_axil.arprot),    // input wire [2 : 0] s_axil_arprot
+        .s_axil_rready                  (m_axil.rready),    // input wire s_axil_rready
+        .s_axil_rvalid                  (m_axil.rvalid),    // output wire s_axil_rvalid
+        .s_axil_rdata                   (m_axil.rdata),      // output wire [63 : 0] s_axil_rdata
+        .s_axil_rresp                   (m_axil.rresp)      // output wire [1 : 0] s_axil_rresp
 
     );
 
